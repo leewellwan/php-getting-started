a7ba7278af4204129bcd32f7245528905b19127b
leewellwan <leewellwandetow@gmail.com>
2022-11-10 13:20:36 +0300

    php page

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello world program";
+
+$myName = "Leewell Wandeto";
+echo $myName;
+var_dump($myName);
+
+// integers
+$a=30;
+$b="30";
+$c= true;
+$d=200.34;
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+echo "My Name is".$myName. "My M-PESA balance is Ksh.".$d;
+?>
